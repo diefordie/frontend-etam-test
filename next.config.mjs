8e1c4d1a93fbe40a15879b36372af9aa0a1414ed
Ardhii <127921311+diefordie@users.noreply.github.com>
2024-12-15 01:03:34 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  webpack(config, { isProduction }) {
+    if (isProduction) {
+      // Disable CSS minimization to debug
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+};
